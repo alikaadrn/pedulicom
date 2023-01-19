7a65f0ff09709ab8013e4adb193529c1427f6f2f
Alika Adriananda <alikaadriananda32@gmail.com>
2023-01-19 21:26:15 +0700

    peduli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {React} from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar  from "./components/Navbar";
+import Artikel from "./components/Artikel";
+import Kampanye from "./components/Kampanye";
+import Program from "./components/Program";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Routes>
+    <Route path="/Artikel" element={<Artikel />} />
+    <Route path="/Kampanye" element={<Kampanye />} />
+    <Route path="/Program" element={<Program />} />
+
+    </Routes>
+    </>
+  )
 }
 
 export default App;
